6039577f634eb6acf1cabb37907ac2fe8eb54175
huynguyenwi <>
2026-09-05 16:40:21 +0700

    second

--- fontend.app.web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace fontend.app.web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- fontend.app.web/Services/Repos/Product_CatService_I.cs
+++
@@ -1,9 +0,0 @@
-﻿using CRM.data;
-
-namespace csdl.web.Services
-{
-    public interface Product_CatService_I
-    {
-        Task<List<vw_Category>> GetAllCate();
-    }
-}
